a184fcac385b5e8f8f1177841fdf1ad1cb6883d1
JAMSHED-ALI-1 <alij0940@gmail.com>
2024-10-04 07:11:34 -0700

    Initial commit

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,33 +1,33 @@
-var _ = require('lodash');	
-var nodemailer = require('nodemailer');
+// var _ = require('lodash');	
+// var nodemailer = require('nodemailer');
 
-var config = {
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: 'jamshed.atafom@gmail.com',
-        pass: 'Mobiloitte@1'
-    }
-};
+// var config = {
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     auth: {
+//         user: 'jamshed.atafom@gmail.com',
+//         pass: 'Mobiloitte@1'
+//     }
+// };
     
-var transporter = nodemailer.createTransport(config);
+// var transporter = nodemailer.createTransport(config);
 
-var defaultMail = {
-    user: 'jamshed.atafom@gmail.com',
-    text: 'test text',
-};
+// var defaultMail = {
+//     user: 'jamshed.atafom@gmail.com',
+//     text: 'test text',
+// };
 
 
-const send = (to, subject, html) => {
-    // use default setting
-    mail = _.merge({html}, defaultMail, to);
+// const send = (to, subject, html) => {
+//     // use default setting
+//     mail = _.merge({html}, defaultMail, to);
     
-    // send email
-    transporter.sendMail(mail, function(error, info){
-        if(error) return console.log(error);
-        console.log('mail sent:', info.response);
-    });
-}
-module.exports = {
-    send
-}
+//     // send email
+//     transporter.sendMail(mail, function(error, info){
+//         if(error) return console.log(error);
+//         console.log('mail sent:', info.response);
+//     });
+// }
+// module.exports = {
+//     send
+// }
